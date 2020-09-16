94e0ec8a22946102cf4899ee24e9bfbd9949cc76
Alex-A-Moraes <Alex-A-Moraes@users.noreply.github.com>
2020-09-16 10:11:16 -0300

    Update LogSystem.ts

--- src/log/LogSystem.ts
+++ src/log/LogSystem.ts
@@ -1,19 +1,20 @@
-import * as fs from "fs";
+import * as fs from 'fs';
+import path from 'path';
 
 class LogSystem {
-  private static fileWrite(message: any) {
-    fs.appendFile("./log.txt", `${message} \n`, function (err) {
-      if (err) throw err;
-    });
-  }
+    private static fileWrite(message: any) {
+        fs.appendFile(path.join(__dirname, '../log.txt'), `${message} \n`, function (err) {
+            if (err) throw err;
+        });
+    }
 
-  public static Error(msg: any) {    
-    this.fileWrite(`Erro [${new Date().toISOString()}] ${msg}`);
-  }
+    public static Error(msg: any) {
+        this.fileWrite(`Erro [${new Date().toISOString()}] ${msg}`);
+    }
 
-  public static Info(msg: any) {    
-    this.fileWrite(`Info [${new Date().toISOString()}] ${msg}`);
-  }
+    public static Info(msg: any) {
+        this.fileWrite(`Info [${new Date().toISOString()}] ${msg}`);
+    }
 }
 
 export default LogSystem;
